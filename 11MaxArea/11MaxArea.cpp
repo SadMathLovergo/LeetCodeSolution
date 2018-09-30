948680776f2f1429b1cd6e0d6092a813ebe7d529
SadMathLovergo <sadmathlover@gmail.com>
2018-09-30 16:04:03 +0800

    提交第11题 盛最多水的容器

--- 11MaxArea/11MaxArea.cpp
+++ 11MaxArea/11MaxArea.cpp
@@ -1,10 +1,21 @@
 #include <vector>
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
 	int maxArea(vector<int>& height) {
-
+		if (height.size() < 2)
+			return 0;
+		int res = 0;
+		for (int i = 0, j = height.size() - 1; i < j;) {
+			res = max(res, min(height[i], height[j])*(j - i));
+			if (height[i] < height[j])
+				i++;
+			else
+				j--;
+		}
+		return res;
 	}
 };
