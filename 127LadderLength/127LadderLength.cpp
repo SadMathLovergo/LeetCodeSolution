7821c4d3acff766aa464c7502e9066c8a9644cb5
SadMathLovergo <sadmathlover@gmail.com>
2018-09-30 16:04:37 +0800

    提交第127题 单词接龙

--- 127LadderLength/127LadderLength.cpp
+++ 127LadderLength/127LadderLength.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 class Solution {
 private:
+	//判断这两个单词是否只差一个字母
 	bool isSimilarWord(string word1, string word2) {
 		int difNum = 0;
 		for (int i = 0; i < word1.size(); i++)
@@ -17,9 +18,9 @@ private:
 			return true;
 		return false;
 	}
-
+	//采用广度优先遍历的方法寻找最短路径
 	int BFS(int startIndex, int endIndex, vector<vector<bool>>& simWordRecord) {
-		queue<pair<int, int>> pathMemory;//[位置，当前步数]
+		queue<pair<int, int>> pathMemory;//(位置，当前步数)
 		pathMemory.push(make_pair(startIndex, 1));
 
 		while (!pathMemory.empty()) {
@@ -44,6 +45,7 @@ public:
 		if (wordList.empty())
 			return 0;
 
+		//判断wordList中是否有endWord，并找出endWord的索引
 		int endWordIndex = -1;
 		for (int i = 0; i < wordList.size(); i++)
 			if (endWord == wordList[i]) {
@@ -53,8 +55,10 @@ public:
 		if (endWordIndex == -1)
 			return 0;
 
+		//将beginWord压入wordList中
 		wordList.push_back(beginWord);
 		int n = wordList.size();
+		//用于记录每两个单词是否可以转化
 		vector<vector<bool>> simWordRecord(n, vector<bool>(n, false));
 		for (int i = 0; i < n; i++) {
 			for (int j = i + 1; j < n; j++) {
@@ -64,7 +68,7 @@ public:
 				}
 			}
 		}
-
+		//采用广度优先遍历的方式，寻找最短路径
 		return BFS(n - 1, endWordIndex, simWordRecord);
 	}
 };
